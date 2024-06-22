8e48b442c4d835ca872df8a2513a9c5617830c71
cear0004 <cear0004@student.monash.edu>
2024-06-23 00:49:03 +1000

    Improved the icons in the list view.

--- frontend/src/components/kanbans/kanban-list-view.tsx
+++ frontend/src/components/kanbans/kanban-list-view.tsx
@@ -26,7 +26,7 @@ export default function KanbanListView() {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M12 6v6h6m-6 0H6"
+            d="M12 6v12m6-6H6"
           />
         </svg>
       </div>
@@ -49,31 +49,31 @@ const BoardCard = ({ title, dueDate }: { title: string; dueDate: string }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth="1.5"
+            strokeWidth={1.5}
             stroke="currentColor"
-            className="w-5 h-5"
+            className="size-6"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M9 13.5l3-3 3 3m0 0l-3 3-3-3m3 3V6.75M6.75 21h10.5a4.5 4.5 0 004.5-4.5V6.75a4.5 4.5 0 00-4.5-4.5H6.75A4.5 4.5 0 002.25 6.75v10.5A4.5 4.5 0 006.75 21z"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
             />
           </svg>
         </button>
       </div>
-      <p className="absolute bottom-4 left-4 text-sm">
+      <p className="absolute bottom-4 left-4 text-sm flex items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
+          strokeWidth={1.5}
           stroke="currentColor"
-          className="inline w-4 h-4 mr-1"
+          className="w-4 h-4 mr-1"
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M12 6v6h6M18 12H6m6 0H3"
+            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
         Due {dueDate}
