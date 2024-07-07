fc0a19b8d468e642157b6a68c6742125ecf3a80c
cear0004 <cear0004@student.monash.edu>
2024-07-08 09:15:19 +1000

    fixed the scrollbar on task detail view

--- frontend/src/components/kanbans/kanban-task-detail.tsx
+++ frontend/src/components/kanbans/kanban-task-detail.tsx
@@ -37,58 +37,66 @@ const DetailedTaskView = ({
   }, [onClose]);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center rounded-lg justify-center bg-black bg-opacity-50">
       <div
         ref={cardRef}
-        className="bg-[#2e223b] text-white rounded-lg p-6 shadow-md w-3/4 text-left"
+        className="bg-[#2e223b] text-white rounded-lg p-6 shadow-md w-3/4 text-left overflow-hidden"
+        style={{ maxHeight: "95vh" }}
       >
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-semibold">{task.name}</h2>
-          <button className="text-red-500" onClick={onClose}>
-            Close
+        <div
+          className="custom-scrollbar overflow-y-auto pb-5"
+          style={{
+            maxHeight: "90vh",
+          }}
+        >
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-semibold">{task.name}</h2>
+            <button className="text-red-500" onClick={onClose}>
+              Close
+            </button>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Urgency:</span>
+            <span
+              className={`w-3 h-3 inline-block mx-2 rounded-full ${urgencyToColour(
+                task.urgency
+              )}`}
+            ></span>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Due date:</span>
+            <span className="ml-2">{task.dueDate}</span>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Description:</span>
+            <p>{task.description}</p>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Assigned to:</span>{" "}
+            <p>
+              {assignedUsers.map((user) => (
+                <span key={user.id} className="mx-2">
+                  {user.username}
+                </span>
+              ))}
+            </p>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Status:</span>{" "}
+            <p>{getTaskStatus(task.taskCategoryId)}</p>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Comments:</span>
+            {/* <p>{task.comments}</p> */}
+          </div>
+          <textarea
+            placeholder="Leave a comment..."
+            className="w-full p-2 rounded"
+          ></textarea>
+          <button className="mt-2 bg-orange-500 text-white p-2 rounded">
+            Comment
           </button>
         </div>
-        <div className="mb-4">
-          <span className="font-semibold">Urgency:</span>
-          <span
-            className={`w-3 h-3 inline-block mx-2 rounded-full ${urgencyToColour(
-              task.urgency
-            )}`}
-          ></span>
-        </div>
-        <div className="mb-4">
-          <span className="font-semibold">Due date:</span>
-          <span className="ml-2">{task.dueDate}</span>
-        </div>
-        <div className="mb-4">
-          <span className="font-semibold">Description:</span>
-          <p>{task.description}</p>
-        </div>
-        <div className="mb-4">
-          <span className="font-semibold">Assigned to:</span>{" "}
-          <p>
-            {assignedUsers.map((user) => (
-              <span key={user.id} className="mx-2">
-                {user.username}
-              </span>
-            ))}
-          </p>
-        </div>
-        <div className="mb-4">
-          <span className="font-semibold">Status:</span>{" "}
-          <p>{getTaskStatus(task.taskCategoryId)}</p>
-        </div>
-        <div className="mb-4">
-          <span className="font-semibold">Comments:</span>
-          {/* <p>{task.comments}</p> */}
-        </div>
-        <textarea
-          placeholder="Leave a comment..."
-          className="w-full p-2 rounded"
-        ></textarea>
-        <button className="mt-2 bg-orange-500 text-white p-2 rounded">
-          Comment
-        </button>
       </div>
     </div>
   );
