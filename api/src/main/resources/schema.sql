d9eefb91f106ff83e457b44e8c37d3aef11837f0
mjas0006 <mjas0006@student.monash.edu>
2024-08-10 16:04:25 +1000

    backend: fix tests

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -1,3 +1,12 @@
+-- Create the "board" table
+CREATE TABLE board (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    start_date DATE,
+    due_date DATE
+);
+
 -- Insert some initial data into the "board" table
 INSERT INTO board (name, description, start_date, due_date)
 VALUES
@@ -5,7 +14,13 @@ VALUES
     ('Board 2', 'Description for Board 2', '2022-02-01', '2022-02-28'),
     ('Board 3', 'Description for Board 3', '2022-03-01', '2022-03-31');
 
+-- Create the "board" table
+CREATE TABLE user_board (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    board_id INT NOT NULL
+);
 
-INSERT INTO UserBoard (id, user_id, board_id) VALUES (1, 1, 1);
-INSERT INTO UserBoard (id, user_id, board_id) VALUES (2, 1, 2);
-INSERT INTO UserBoard (id, user_id, board_id) VALUES (3, 1, 3);
+INSERT INTO user_board (id, user_id, board_id) VALUES (1, 1, 1);
+INSERT INTO user_board (id, user_id, board_id) VALUES (2, 1, 2);
+INSERT INTO user_board (id, user_id, board_id) VALUES (3, 1, 3);
