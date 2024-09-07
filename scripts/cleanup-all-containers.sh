97c22e566d3553dad078653a5c8e9e4540b31ba8
mjas0006 <mjas0006@student.monash.edu>
2024-09-07 20:05:30 +1000

    Fix naming of docker file

--- scripts/cleanup-all-containers.sh
+++ scripts/cleanup-all-containers.sh
@@ -2,7 +2,7 @@
 
 # Stop and remove containers, networks, and volumes
 docker compose -f docker-compose.fullstack.yml down -v
-docker compose -f docker-compose.frontend-dev.yml down -v
+docker compose -f docker-compose.frontend-dev-mode.yml down -v
 docker compose -f docker-compose.backend-only.yml down -v
 
 # Optionally, remove dangling volumes not associated with any container
