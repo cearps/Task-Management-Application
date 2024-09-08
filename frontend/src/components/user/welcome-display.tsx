53809b832f4295e1eb1a9275f24c77f557842170
cear0004 <cear0004@student.monash.edu>
2024-09-09 09:47:36 +1000

    Fixed redirects.

--- frontend/src/components/user/welcome-display.tsx
+++ frontend/src/components/user/welcome-display.tsx
@@ -4,27 +4,6 @@ import { User } from "../../utilities/types";
 import { useNavigate } from "react-router-dom";
 
 export default function WelcomeDisplay() {
-  const [user, setUser] = useState(null as User | null);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const subscription = UserAPI.getUserObservable().subscribe({
-      next: (response) => {
-        setUser(response);
-      },
-      error: (error) => {
-        // redirect to sign up page
-        navigate("/accounts/signup");
-        console.error("Error fetching user:", error);
-      },
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
-
   return (
     <div>
       <h1>Welcome {user?.userTag}</h1>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,38 @@
 import Base from "../components/sections/base";
-import WelcomeDisplay from "../components/user/welcome-display";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import UserAPI from "../api/userAPI";
+// import WelcomeDisplay from "../components/user/welcome-display";
 
 export default function Home() {
+  const [user, setUser] = useState(null as User | null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const subscription = UserAPI.getUserObservable().subscribe({
+      next: (response) => {
+        setUser(response);
+        navigate("/boards");
+      },
+      error: (error) => {
+        // clear token
+        localStorage.removeItem("token");
+        // redirect to sign up page
+        navigate("/accounts/signup");
+        console.error("Error fetching user:", error);
+      },
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
   return (
     <Base pageTitle="Home">
       <>
-        <h1>Home</h1>
-        <WelcomeDisplay />
+        {/* <h1>Home</h1>
+        <WelcomeDisplay /> */}
       </>
     </Base>
   );
