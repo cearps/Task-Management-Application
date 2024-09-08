bc2c346b57dcc7fe73c876e60ceb3014ea2e306d
mjas0006 <mjas0006@student.monash.edu>
2024-09-09 09:06:26 +1000

    Removing stacktrace from looging

--- api/src/main/java/com/backend/backend/exceptions/GlobalExceptionHandler.java
+++ api/src/main/java/com/backend/backend/exceptions/GlobalExceptionHandler.java
@@ -47,7 +47,7 @@ public class GlobalExceptionHandler {
         }
 
         if (errorDetail == null) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), "Check logs for more information");
             errorDetail.setProperty("description", "Unknown internal server error.");
         }
 
