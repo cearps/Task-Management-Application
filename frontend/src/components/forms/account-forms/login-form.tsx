2112d8d22693acff27ae0fbe92fbb71d1a167ccd
cear0004 <cear0004@student.monash.edu>
2024-09-23 15:08:47 +1000

    responsive changes on the login/signup forms.

--- frontend/src/components/forms/account-forms/login-form.tsx
+++ frontend/src/components/forms/account-forms/login-form.tsx
@@ -39,7 +39,7 @@ const LogInForm = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-full">
       <FormBase onSubmit={handleLogin}>
         <h2 className="text-2xl font-bold mb-6">Login</h2>
         <Field
