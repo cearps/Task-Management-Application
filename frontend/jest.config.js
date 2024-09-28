f44e8f5ebd7eec0c5d4e841d6fe5ba00b4960439
cear0004 <cear0004@student.monash.edu>
2024-09-28 21:38:09 +1000

    Updated jest config for coverage threshold.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,4 +10,14 @@ export default {
   },
 
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  // Other Jest configuration
+  collectCoverage: true, // Enable coverage collection
+  coverageThreshold: {
+    global: {
+      statements: 80, // 80% of statements must be covered
+      branches: 70, // 70% of branches must be covered
+      functions: 85, // 85% of functions must be covered
+      lines: 80, // 80% of lines must be covered
+    },
+  },
 };
