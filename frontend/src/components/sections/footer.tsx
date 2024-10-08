6155faa2bca80f0673f06354341235dca297272e
cear0004 <cear0004@student.monash.edu>
2024-10-08 14:35:37 +1100

    added button to exit tutorial.

--- frontend/src/components/sections/footer.tsx
+++ frontend/src/components/sections/footer.tsx
@@ -1,4 +1,8 @@
 const Footer = () => {
+  const handleExitTutorial = () => {
+    localStorage.setItem("firstLogin", "false");
+    window.location.reload();
+  };
   return (
     <footer
       className="bg-gray-800 text-white p-4 mt-8 fixed bottom-0 w-full flex items-center shadow-md"
@@ -18,6 +22,17 @@ const Footer = () => {
           </a>
         </nav> */}
       </div>
+      {localStorage.getItem("firstLogin") === "true" && (
+        <div className="flex items-center">
+          <p className="text-sm mr-2">Currently in tutorial mode</p>
+          <button
+            className="text-sm text-white bg-red-500 px-2 py-1 rounded hover:bg-red-600"
+            onClick={handleExitTutorial}
+          >
+            Exit Tutorial
+          </button>
+        </div>
+      )}
     </footer>
   );
 };
