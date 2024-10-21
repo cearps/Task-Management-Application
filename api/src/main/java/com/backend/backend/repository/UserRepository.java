67d5cd0bbd0025727e1f29166068f8cedf2522c8
mjas0006 <mjas0006@student.monash.edu>
2024-10-22 10:14:35 +1100

    Commenting: Adding acknowledgement for user authentication tutorial

--- api/src/main/java/com/backend/backend/repository/UserRepository.java
+++ api/src/main/java/com/backend/backend/repository/UserRepository.java
@@ -5,6 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Acknowledgements:
+ * This class was written by following the instructions in the "Implement JWT authentication
+ * in a Spring Boot 3 application" tutorial on Medium published by Eric C. Togo.
+ * The tutorial was published on 20/03/2024.
+ *
+ * The full APA 7th reference is:
+ * Tiogo, E.C. (2024) Implement JWT authentication in a Spring Boot 3
+ *     application, Medium. Available at:
+ *     https://medium.com/@tericcabrel/implement-jwt-authentication-in-a-spring-boot-3-application-5839e4fd8fac
+ *     (Accessed: 22 June 2024).
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUserTag(String userTag);
